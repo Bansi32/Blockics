5aa486625caac33379ef07f637718a43354895ec
Anshuman Padiya <53999212+AnshumanPadiya@users.noreply.github.com>
2023-02-24 21:12:41 +0530

    added new pages

--- Frontend/nfttemplate/pages/index.js
+++ Frontend/nfttemplate/pages/index.js
@@ -1,29 +1,52 @@
 import React from "react";
+
+//INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection, Service, Subscribe, Title, Category, Filter, AudioLive, FollowerTab,Slider, Brand} from "../components/componentindex";
-import {BigNFTSlider} from "../components/componentindex";
+import {
+  HeroSection,
+  Service,
+  BigNFTSilder,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+  NFTCard,
+  Collection,
+  AudioLive,
+  FollowerTab,
+  Slider,
+  Brand,
+  Video,
+} from "../components/componentsindex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
-      <BigNFTSlider/>
+      <BigNFTSilder />
+      <Title
+        heading="Audio Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <AudioLive />
+      <FollowerTab />
+      <Slider />
+      <Collection />
       <Title
         heading="Featured NFTs"
         paragraph="Discover the most outstanding NFTs in all topics of life."
       />
       <Filter />
-      <AudioLive/>
-      <FollowerTab />
-      <Slider />
+      <NFTCard />
       <Title
         heading="Browse by category"
         paragraph="Explore the NFTs in the most featured categories."
       />
-      <Category/>
-      <Subscribe/>
+      <Category />
+      <Subscribe />
       <Brand />
+      <Video />
     </div>
   );
 };
